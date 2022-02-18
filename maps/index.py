3263d4a2df2ba4b666aab6008e6624529d41e6e4
Philippe Nguyen <philnguyen94@gmail.com>
2022-02-17 22:10:41 -0800

    add states map

--- maps/index.py
+++ maps/index.py
@@ -9,7 +9,11 @@ from dash.dependencies import Input, Output
 import dash_bootstrap_components as dbc
 
 
-PATH = pathlib.Path(__file__).parent
+DATA_PATH = pathlib.Path(__file__).parent.joinpath('./data').resolve()
+DATA_PREFIX = {
+    "World": "countries_",
+    "United States": "states_",
+}
 DATA_NAMES = {
     "band_num": "band_num.csv",
     "album_num": "album_num.csv",
@@ -24,6 +28,8 @@ FEATURES = {
     "review_avg": "Average review score",
     "review_weighted": "Weighted-average review score",
 }
+DATE_RANGE = (1970, 2022)
+DATE_INTERVAL = 10
 
 app = dash.Dash(
     __name__,
@@ -47,7 +53,15 @@ app.layout = html.Div([
         html.A('Genre predictor', href='https://metal-lyrics-genre-classifier.herokuapp.com/'),
     ]),
     html.Div([
-        html.P("Show value:", style={'display': 'inline-block', 'margin-right': 20}),
+        html.P("Region:", style={'display': 'inline-block', 'margin-right': 20}),
+        dbc.RadioItems(
+            id='radio',
+            options=[{'value': x, 'label': x} for x in DATA_PREFIX.keys()],
+            value=list(DATA_PREFIX.keys())[0],
+            inline=True,
+            style={'display': 'inline-block', 'margin-right': 50},
+        ),
+        html.P("Value:", style={'display': 'inline-block', 'margin-right': 20}),
         html.Div([
             dcc.Dropdown(
                 id='dropdown',
@@ -56,16 +70,16 @@ app.layout = html.Div([
                 clearable=False,
                 style={'width': 300, 'background-color': '#111111', 'verticalAlign': 'middle'},
             ),
-        ], style={'display': 'inline-block', 'verticalAlign': 'middle', 'margin-right': 40}, className='dropdown'),
+        ], style={'display': 'inline-block', 'verticalAlign': 'middle', 'margin-right': 50}, className='dropdown'),
         html.P("Date range:", style={'display': 'inline-block', 'margin-right': 20}),
         html.Div([
             dcc.RangeSlider(
                 id='slider',
-                min=1970,
-                max=2022,
+                min=DATE_RANGE[0],
+                max=DATE_RANGE[1],
                 step=1,
-                value=[1970, 2022],
-                marks={i: str(i) for i in range(1970, 2022, 10)},
+                value=DATE_RANGE,
+                marks={i: str(i) for i in range(DATE_RANGE[0], DATE_RANGE[1], DATE_INTERVAL)},
                 tooltip={'always_visible': True}
             )
         ], style={'width': 400, 'display': 'inline-block', 'verticalAlign': 'bottom'}),
@@ -74,13 +88,25 @@ app.layout = html.Div([
 ], style={'margin': 10})
 
 
-@app.callback(Output("choropleth", "figure"), [Input("dropdown", "value"), Input("slider", "value")])
-def display_choropleth(feature, date_range):
-    df_path = PATH.joinpath('./data').resolve().joinpath("countries_" + DATA_NAMES[feature])
+@app.callback(
+    Output("choropleth", "figure"),
+    [Input("radio", "value"),
+     Input("dropdown", "value"),
+     Input("slider", "value")]
+)
+def display_choropleth(region, feature, date_range):
+    basename = DATA_PREFIX[region] + DATA_NAMES[feature]
+    df_path = DATA_PATH.joinpath(basename)
     df = pd.read_csv(df_path, index_col=0)
+    if region == 'World':
+        locationmode = 'country names'
+        scope = 'world'
+    elif region == 'United States':
+        locationmode = 'USA-states'
+        scope = 'usa'
     kwargs = dict(
         locations=df.columns,
-        locationmode="country names",
+        locationmode=locationmode,
         colorscale='viridis',
     )
     start, end = date_range
@@ -120,6 +146,7 @@ def display_choropleth(feature, date_range):
         margin=dict(l=0, r=0, t=0, b=0),
     )
     fig.update_geos(
+        scope=scope,
         showland=True, landcolor="#111111",
         showocean=True, oceancolor="#111111",
         showlakes=True, lakecolor="#111111",
